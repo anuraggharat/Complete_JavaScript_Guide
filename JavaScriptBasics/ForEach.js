275f5b7c447d118c81d73ca1a890d2dc15517b8f
Anurag Gharat <anuraggharat55@gmail.com>
2023-08-10 12:02:55 +0530

    Added For Each method

--- JavaScriptBasics/ForEach.js
+++ JavaScriptBasics/ForEach.js
@@ -1,5 +1,15 @@
-// for each is a method and not a loop
+// for each is an array method and not a loop
 
 const arr = [10,20,30,40,50]
 
 arr.forEach(i=>console.log(i*i))
+
+
+const obj = {
+    name:"Anurag",
+    age:24,
+    role:"SDE"
+}
+
+// for each cannot be used on object
+obj.forEach(i=>console.log(i))
