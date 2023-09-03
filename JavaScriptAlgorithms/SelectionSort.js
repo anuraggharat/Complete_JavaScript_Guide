7b613ec6f4dfa9c0868e51c132459fff3a44b0ba
Anurag Gharat <anuraggharat55@gmail.com>
2023-09-03 16:18:39 +0530

    added Selection Sort

---
+++ JavaScriptAlgorithms/SelectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(array){
+    let min;
+    for (let i = 0; i < array.length; i++) {
+        min = i
+        for (let j = i+1; j < array.length; j++) {
+            if (array[j]<=array[min]) {
+                min = j
+            }
+        }
+        let temp = array[i]
+        array[i] = array[min]
+        array[min] = temp
+    }
+}
+
+const array=[64,22,51,69,11,27,42]
+selectionSort(array)
+console.log(array)
+//O(nXn)
